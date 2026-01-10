3d9fdcf64537e63103da94831b096d901e3b4a67
nikhilhonkalaskar-coder <nikhilhonkalaskar@gmail.com>
2026-01-10 13:05:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ const db = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "ebiztech99",
   database: process.env.DB_NAME || "tushar_bumkar_institute_database",
-  waitForConnections: true,
-  connectionLimit: 10,
 });
 
 /* ================== RAW BODY (for signature verification) ================== */
@@ -171,4 +169,5 @@ app.get("/razorpay-webhook", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
