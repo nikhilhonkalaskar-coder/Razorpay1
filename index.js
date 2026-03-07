83692a746b01079f922a4b106a04a6b603be5678
nikhilhonkalaskar-coder <nikhilhonkalaskar@gmail.com>
2026-03-07 17:51:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 10000;
 /* ===== SUPABASE DATABASE CONNECTION ===== */
 
 const connectionString =
-"postgresql://postgres.rdutjyuqvnzkgjodamue:m8Z3XgBu1owxSHfQ@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require";
+"postgresql://postgres.rdutjyuqvnzkgjodamue:m8Z3XgBu1owxSHfQ@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?pgbouncer=true";
 
 const pool = new Pool({
   connectionString: connectionString,
@@ -109,4 +109,5 @@ app.post("/webhook", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
